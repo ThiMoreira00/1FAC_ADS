3cd896684d8b1f768c75e689b708dd06b29b4c52
Thiago M <129688942+ThiMoreira00@users.noreply.github.com>
2023-07-13 11:48:55 -0300

    Create [1FAC] Questão 2 - Verificar ordenação dos algarismos.c

---
+++ [1FAC] 2023.1/[1FAC] Avaliação II/[1FAC] Questão 2 - Verificar ordenação dos algarismos.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int verificarCrescente (int n);
+
+void main() {
+	
+	switch(verificarCrescente(4)) { 
+		case 1: printf("Estao ordenados crescentemente.");
+				break;
+		case 0: printf("Nao estao ordenados crescentemente.");
+	}
+	
+}
+
+int verificarCrescente (int n) {
+	
+	int anterior, resto;
+	int numero = n;
+	
+	if(n < 10) {
+		return 1;
+	}
+	else {
+
+		do {
+			anterior = n%10;
+			n /= 10;
+			resto = n%10;
+			
+			if(resto > anterior) { 
+				return 0;
+			}
+			
+		} while (n > 10);
+		return 1; 
+		
+	}
+}
